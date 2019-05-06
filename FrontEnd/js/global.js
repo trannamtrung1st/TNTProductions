c572afddd54751e75711593f1d64ed85e5a77be2
trannamtrung1st <trannamtrung1st@gmail.com>
2019-05-06 20:12:26 +0700

    no message

--- FrontEnd/js/global.js
+++
@@ -1,9 +0,0 @@
-var root = "file:///T:/TNT/Workspace/TNTProductions/FrontEnd/";
-
-var pagesContent = [];
-for (var i = 0; i < 10; i++) {
-    pagesContent[i] =
-        '<h1 style="color:' + (i % 2 == 0 ? 'red' : 'blue') + '">' +
-        'Content of page ' + i +
-        '</h1>';
-}
--- PromoterArea/PosRefactor.sql
+++
@@ -1,6 +0,0 @@
-﻿ALTER TABLE PromotionDetal
-ADD Description nvarchar(MAX)
-
-ALTER TABLE Promotion
-ADD AllowApplyWithOthers bit
-EXEC sp_RENAME 'PromotionDetail.RegExCode', 'MembershipCodePattern', 'COLUMN';
