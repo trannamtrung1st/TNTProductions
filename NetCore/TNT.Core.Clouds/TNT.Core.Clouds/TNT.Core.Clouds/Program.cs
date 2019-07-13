85a98d593da25ff3732606f77d4ae68357f9c364
trannamtrung1st <trannamtrung1st@gmail.com>
2019-07-13 18:01:58 +0700

    Clouds

---
+++ NetCore/TNT.Core.Clouds/TNT.Core.Clouds/TNT.Core.Clouds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using TNT.Core.Clouds.Firebase;
+
+namespace TNT.Core.Clouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new FirebaseClient(
+                "s",
+                "s");
+
+            var test = client.CreateDeviceGroup("test_group", new List<string>()
+            {
+                "TNT"
+            }).Result;
+            var testResp = test.Content.ReadAsStringAsync();
+        }
+    }
+}
