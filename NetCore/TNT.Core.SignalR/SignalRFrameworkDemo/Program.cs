e9b4a4952d58e9df97bda451c1e6d116602b8b74
trannamtrung1st <trannamtrung1st@gmail.com>
2019-11-03 10:40:39 +0700

    HttpListener

---
+++ NetCore/TNT.Core.SignalR/SignalRFrameworkDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:8080/");
+                string content = "true";
+                listener.Start();
+                while (!string.IsNullOrWhiteSpace(content))
+                {
+                    var context = listener.GetContext();
+                    var req = context.Request;
+                    var res = context.Response;
+
+                    using (var reader = new StreamReader(req.InputStream))
+                    {
+                        content = reader.ReadToEnd();
+                        Console.WriteLine(content);
+                    }
+                    using (res)
+                    {
+                        res.StatusCode = 200;
+                        var writer = new StreamWriter(res.OutputStream);
+                        writer.WriteLine(content);
+                        writer.Flush();
+                    }
+                }
+            }
+        }
+    }
+}
