102336a0cc1f19d34697c5d23639a161c18221f4
trannamtrung1st <trannamtrung1st@gmail.com>
2020-04-24 20:45:45 +0700

    upgrade to 3.1

---
+++ NetCore/TNT.Core.Http/TestWeb31/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TNT.Core.Helpers.DI;
+using TNT.Core.Http.DI;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestWeb31.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [InjectionFilter]
+    public class ValuesController : ControllerBase
+    {
+        [Inject]
+        private readonly IWebHostEnvironment _env;
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NetCore/TNT.Core.Http/TestWeb31/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using TNT.Core.Helpers.DI;
+using TNT.Core.Http.DI;
+
+namespace TestWeb31.Pages
+{
+    [InjectionFilter]
+    public class IndexModel : PageModel
+    {
+        [Inject]
+        private readonly IWebHostEnvironment _env;
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
